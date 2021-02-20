a55db3a9272948760e20d8bddf7e82c001050670
MohamedHefnawy <77691933+MohamedHefnawy@users.noreply.github.com>
2021-02-21 01:07:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,129 +1,62 @@
-* {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-    background-color: 0303030;
-    text-decoration: none;
-    font: 1em sans-serif;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+let search = document.querySelector('.input .search-box');
+let searchBtn = document.querySelector("body button");
+let menu = document.querySelector("menu li");
+search.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+        if (search.value.trim() != 0) {
+            getResults(search.value);
+            console.log(search.value);
+        } else {
+            console.log('else');
+            alert('you have to enter something ');
+        }
+    }
 }
 
-body .nav {
-    background-color: rgb(199, 85, 85);
-    width: 100%;
-    height: 60px;
-    margin-top: 0;
-    padding-top: 10px;
-    padding-left: 30px;
-    padding-bottom: 10px;
-    color: white;
-    font-size: x-large;
-    text-align: left;
-    font-weight: 900;
+searchBtn.onclick = () => {
+
+    if (search.value.trim() != 0) {
+        getResults(search.value);
+        console.log(search.value);
+    } else {
+        console.log('else');
+        alert('you have to enter something ');
+    }
 }
 
-.form {
-    padding: 30px 0;
-}
+function getResults(query) {
 
-.search-box {
-    width: 100%;
-    font-weight: 600;
-    padding: 10px;
-    color: black;
-    background-color: white;
-    border: 1px solid #ccc;
-}
+    console.log(query);
+    fetch(`https://api.github.com/users/${query}`)
+        .then(res => {
+            return res.json();
+        }).then(data => displatResults(data));
 
-body .btn {
-    width: 100%;
-    color: white;
-    background: #333;
-    margin: 1.2rem 0;
-    cursor: pointer;
-}
+};
 
-body .container-1 {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-}
 
-body .btn:hover {
-    background: gray;
-    color: white;
-    transition: all 0.3s ease;
-}
+function displatResults(res) {
+    if (res.message == "Not Found") {
+        alert(' unkown user');
 
-form.container ul {
-    align-items: center;
-    text-align: center;
-    height: 300px;
-    justify-content: space-between;
-    margin: 10px auto;
-    max-width: 650px;
-    max-height: 300px;
-    border: 1mm;
-    border-radius: 5px;
-    padding: 10px;
-}
+    } else {
 
-form.container ul.ul {
-    border: 0.05mm;
-    border-style: solid;
-    border-color: darkgray;
-    list-style-type: none;
-}
+        let userinfo = document.querySelector('.userinfo ');
+        userinfo.innerHTML = `<h1> UserName: ${res.login} </h1>`;
 
-form.container ul.ul li.avatarurl {
-    float: left;
-}
+        let avatarurl2 = document.querySelector('.avatarurl');
+        avatarurl2.innerHTML = `<img src="${res.avatar_url}" width="150" height="150" alt="Avatar" style="border-radius: 50%;"/>`;
 
-form.container ul.ul li.userinfo {
-    display: flex;
-    margin: 10px;
-    justify-content: center;
-    text-size-adjust: large;
-}
+        let repos = document.querySelector('.repo');
+        repos.innerHTML = `<h3> publicRepos : ${res.public_repos} </h3>`;
 
-ul.ul li.button1 {
-    display: flex;
-    justify-content: center;
-}
+        document.querySelector('.followers').innerHTML = `<h3> followers: ${res.followers} </h3>`;
 
-menu li {
-    float: left;
-    list-style: none;
-    margin-right: 10px;
-    border: 0.05mm;
-    border-style: solid;
-    border-color: darkgray;
-    padding: 4px;
-    border-radius: 2mm;
-}
-
-menu li.repo {
-    background-color: rgb(199, 85, 85);
-    color: white;
-}
-
-menu li.followers {
-    background-color: rgb(85, 199, 119);
-    color: white;
-}
-
-menu li.following {
-    background-color: rgb(255, 255, 255);
-}
-
-menu li.public_gists {
-    background-color: rgb(0, 0, 0);
-    color: white;
-}
-
-.menus {
-    display: flex;
-    justify-content: center;
+        document.querySelector('.following').innerHTML = `<h3> following: ${res.following} </h3>`;
+        document.querySelector('.public_gists').innerHTML = `<h3> public_gists: ${res.public_gists} </h3>`;
+        document.querySelector('.container .button1').innerHTML = `<input type="button" class="btn1" style="background-color: black; color:white; cursor: pointer;" onclick="window.location.href= '${res.html_url}';" value="Visit GitHub profile" />
+    `;
+    }
 }
